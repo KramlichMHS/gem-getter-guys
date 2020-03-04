bdbde8e0845b2aef0232086841c4cc15cdbc79e1
rob65307 <60553106+rob65307@users.noreply.github.com>
2020-03-04 14:32:21 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+namespace SpriteKind {
+    export const Player1 = SpriteKind.create()
+    export const Player2 = SpriteKind.create()
+    export const prj1 = SpriteKind.create()
+    export const prj2 = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -227,27 +233,155 @@ e e e e e e e e e e e e e e e 5
 5 e e e e e e e e e e e e e e e 
 5 e e e e e e e e e e e e e e e 
 `
+    //% blockIdentity=images._tile
+    export const tile12 = img`
+d d d d d d d d d d d d d d d 2 
+d d d d d d d d d 2 d d d d d 2 
+d d d d d d d d d d d d d d d 2 
+d d 2 d d d d d d d d d d d d 2 
+d d d d d d d 2 d d d d d d d 2 
+d d d d d d d d d d d d 2 d d 2 
+d d d d d d d d d d d d d d d 2 
+d d d d d d d d 2 d d d d d d 2 
+d d 2 d d d d d d d d d d d d 2 
+d d d d d d d d d d d d d 2 d 2 
+d d d d d d d d d d d d d d d 2 
+d d d d d d d d d d d d d d d 2 
+d d d d d d d d d d d d 2 d d 2 
+d d 2 d d d 2 d d d d d d d d 2 
+d d d d d d d d d d d d d d d 2 
+d d d d d d d d d d d d d d d 2 
+`
+    //% blockIdentity=images._tile
+    export const tile13 = img`
+8 d d d d d d d d d d d d d d d 
+8 d d d d d d d d 8 d d d d d d 
+8 d d d d d d d d d d d d d d d 
+8 d 8 d d d d d d d d d d d d d 
+8 d d d d d d 8 d d d d d d d d 
+8 d d d d d d d d d d d 8 d d d 
+8 d d d d d d d d d d d d d d d 
+8 d d d d d d d 8 d d d d d d d 
+8 d 8 d d d d d d d d d d d d d 
+8 d d d d d d d d d d d d 8 d d 
+8 d d d d d d d d d d d d d d d 
+8 d d d d d d d d d d d d d d d 
+8 d d d d d d d d d d d 8 d d d 
+8 d 8 d d d 8 d d d d d d d d d 
+8 d d d d d d d d d d d d d d d 
+8 d d d d d d d d d d d d d d d 
+`
 }
-let player1 = sprites.create(img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 8 8 f f f . . . . 
-. . . f f f 8 8 8 8 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 8 8 8 8 8 8 e e f . . 
-. . f e 8 f f f f f f 8 e f . . 
-. . f f f f e e e e f f f f . . 
-. f f e f b f 4 4 f b f e f f . 
-. f e e 4 1 f d d f 1 4 e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f 8 8 8 8 8 8 f 4 e . . 
-. . 4 d f 8 8 8 8 8 8 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`, SpriteKind.Player)
-player1.setPosition(10, 60)
-let player2 = sprites.create(img`
+sprites.onOverlap(SpriteKind.Player2, SpriteKind.prj1, function (sprite, otherSprite) {
+    for (let index = 0; index < info.player2.score(); index++) {
+        gem = sprites.create(img`
+. . . . f f . . . . 
+. . . f 9 9 f . . . 
+. . f 9 9 9 9 f . . 
+. f 9 9 9 9 1 9 f . 
+f 9 9 9 9 1 1 1 9 f 
+f 9 9 1 9 9 1 9 9 f 
+f 9 9 9 9 9 9 9 9 f 
+f 9 9 9 9 9 9 9 9 f 
+f 9 9 1 9 9 9 1 9 f 
+f 9 1 1 1 9 9 9 9 f 
+. f 9 1 9 9 9 9 f . 
+. . f 9 9 9 9 f . . 
+. . . f 9 9 f . . . 
+. . . . f f . . . . 
+`, SpriteKind.Food)
+        gem.setPosition(Math.randomRange(player2.x - 5, player2.x + 5), Math.randomRange(player2.y - 5, player2.x + 5))
+    }
+    info.player2.setScore(0)
+    player2.startEffect(effects.fire, 200)
+    player2.setPosition(150, Math.randomRange(10, 110))
+})
+controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    projectile1 = sprites.createProjectileFromSprite(img`
+. 2 2 2 2 2 . 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+. 2 2 2 2 2 . 
+`, player1, -150, 0)
+    projectile1.setKind(SpriteKind.prj1)
+})
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+. 8 8 8 8 8 . 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+. 8 8 8 8 8 . 
+`, player2, -150, 0)
+    projectile2.setKind(SpriteKind.prj2)
+})
+sprites.onOverlap(SpriteKind.Player2, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fountain, 200)
+    info.player2.changeScoreBy(1)
+})
+controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+    projectile2 = sprites.createProjectileFromSprite(img`
+. 8 8 8 8 8 . 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 
+. 8 8 8 8 8 . 
+`, player2, 150, 0)
+    projectile2.setKind(SpriteKind.prj2)
+})
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    projectile1 = sprites.createProjectileFromSprite(img`
+. 2 2 2 2 2 . 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 
+. 2 2 2 2 2 . 
+`, player1, 150, 0)
+    projectile1.setKind(SpriteKind.prj1)
+})
+sprites.onOverlap(SpriteKind.Player1, SpriteKind.prj2, function (sprite, otherSprite) {
+    for (let index = 0; index < info.player1.score(); index++) {
+        gem = sprites.create(img`
+. . . . f f . . . . 
+. . . f 9 9 f . . . 
+. . f 9 9 9 9 f . . 
+. f 9 9 9 9 1 9 f . 
+f 9 9 9 9 1 1 1 9 f 
+f 9 9 1 9 9 1 9 9 f 
+f 9 9 9 9 9 9 9 9 f 
+f 9 9 9 9 9 9 9 9 f 
+f 9 9 1 9 9 9 1 9 f 
+f 9 1 1 1 9 9 9 9 f 
+. f 9 1 9 9 9 9 f . 
+. . f 9 9 9 9 f . . 
+. . . f 9 9 f . . . 
+. . . . f f . . . . 
+`, SpriteKind.Food)
+        gem.setPosition(Math.randomRange(player1.x - 5, player1.x + 5), Math.randomRange(player1.y - 5, player1.x + 5))
+    }
+    info.player1.setScore(0)
+    player1.startEffect(effects.fire, 200)
+    player1.setPosition(10, Math.randomRange(10, 110))
+})
+sprites.onOverlap(SpriteKind.Player1, SpriteKind.Food, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fountain, 200)
+    info.player1.changeScoreBy(1)
+})
+let projectile2: Sprite = null
+let projectile1: Sprite = null
+let gem: Sprite = null
+let player2: Sprite = null
+let player1: Sprite = null
+player1 = sprites.create(img`
 . . . . . . f f f f . . . . . . 
 . . . . f f f 2 2 f f f . . . . 
 . . . f f f 2 2 2 2 f f f . . . 
@@ -264,14 +398,33 @@ let player2 = sprites.create(img`
 . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
 . . . . . f f f f f f . . . . . 
 . . . . . f f . . f f . . . . . 
-`, SpriteKind.Player)
+`, SpriteKind.Player1)
+player1.setPosition(10, 60)
+player2 = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 8 8 f f f . . . . 
+. . . f f f 8 8 8 8 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 8 8 8 8 8 8 e e f . . 
+. . f e 8 f f f f f f 8 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 8 8 8 8 8 8 f 4 e . . 
+. . 4 d f 8 8 8 8 8 8 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.Player2)
 player2.setPosition(150, 60)
 player1.setFlag(SpriteFlag.StayInScreen, true)
 player2.setFlag(SpriteFlag.StayInScreen, true)
 controller.moveSprite(player1, 75, 75)
 controller.player2.moveSprite(player2, 75, 75)
 tiles.setTilemap(tiles.createTilemap(
-            hex`0a000800050404040404040404060504070404040407040605040704070704070406050404040c0904040406050404040b0a04040406050407040707040704060504070404040407040605040404040404040406`,
+            hex`0a0008000d04040404040404040e0d04070404040407040e0d04070407070407040e0d0404040c090404040e0d0404040b0a0404040e0d04070407070407040e0d04070404040407040e0d04040404040404040e`,
             img`
 . . . . . . . . . . 
 . . 2 . . . . 2 . . 
@@ -282,6 +435,35 @@ tiles.setTilemap(tiles.createTilemap(
 . . 2 . . . . 2 . . 
 . . . . . . . . . . 
 `,
-            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.dungeon.floorLight0,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11],
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.dungeon.floorLight0,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13],
             TileScale.Sixteen
         ))
+info.player2.setScore(0)
+info.player1.setScore(0)
+game.onUpdateInterval(10000, function () {
+    gem = sprites.create(img`
+. . . . f f . . . . 
+. . . f 9 9 f . . . 
+. . f 9 9 9 9 f . . 
+. f 9 9 9 9 1 9 f . 
+f 9 9 9 9 1 1 1 9 f 
+f 9 9 1 9 9 1 9 9 f 
+f 9 9 9 9 9 9 9 9 f 
+f 9 9 9 9 9 9 9 9 f 
+f 9 9 1 9 9 9 1 9 f 
+f 9 1 1 1 9 9 9 9 f 
+. f 9 1 9 9 9 9 f . 
+. . f 9 9 9 9 f . . 
+. . . f 9 9 f . . . 
+. . . . f f . . . . 
+`, SpriteKind.Food)
+    gem.setPosition(Math.randomRange(65, 95), Math.randomRange(45, 75))
+})
+forever(function () {
+    if (info.player2.score() >= 5) {
+        game.over(true, effects.splatter)
+    }
+    if (info.player1.score() >= 5) {
+        game.over(true, effects.confetti)
+    }
+})
